54acd094c772086498667d9da6393216b8ab0bc7
Aliulilun <161181632+Aliulilun@users.noreply.github.com>
2024-03-14 17:17:43 +0800

    main.c

--- main.c
+++ main.c
@@ -12,6 +12,31 @@ printf("employee age = %d\n", emp.age);
 printf(" employee salary = %02f\n\n", emp.salary);
 }
 
+int emp_aveage_age(employee_t emp[], int n) {
+int sum =0；
+for (int i=0;i<3;i++) {
+sum+=emp[i]. age;
+｝
+return sum/3;
+｝
+void emp_writefile(employee_t emp[], int n)
+FILE* fp = fopen(" employee.bin",
+"wb+"）；
+for (int i=0;i<n;i++) {
+fwrite(&emp[i], sizeof(employee_t), 1, fp);
+fclose(fp);
+return;
+}
+void emp_readfile(employee_t emp]) {
+FILE* fp = fopen(" employee.bin", "rb")
+"rb"）；
+int i = 0;
+while (fread(&emp[i], sizeof(employee_t), 1, fp)) {
+printf("[%d] %d %s\n", i, emp[i].id, emp[i]. name);
+｝
+fclose(fp);
+return;
+｝
 int main {
 employee_t emp[3];
 emp[0]. id = 1;
