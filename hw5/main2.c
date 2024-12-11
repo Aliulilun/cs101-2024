e0a0805913cb37be8329c70168e62c528f523069
Aliulilun <161181632+Aliulilun@users.noreply.github.com>
2024-12-11 23:49:08 +0800

    main2.c

--- hw5/main2.c
+++ hw5/main2.c
@@ -2,19 +2,20 @@
 #include <math.h>
 
 int main() {
-    double pi = 0.0; // Variable to store calculated pi
-    double term;     // Current term 
+    double pi = 0.0;    // Variable to store calculated pi
+    double term;        // Current term
     double sign = 1;    // Alternating sign: +1 or -1
-    double x = 0;       // Number of terms
+    int x;              // Number of terms (integer for `for` loop)
 
-    do {
+    for (x = 0; fabs(pi - 3.14159) >= 0.000001; x++) { // fabs:make the positive value 
         term = 4.0 / (2 * x + 1); // Calculate the current term
         pi += sign * term;        // Add or subtract the term
-        sign = -sign;            
-        x++;                      
-    } while (fabs(pi - 3.14159) >= 0.000001); // fabs:make the positive value 
+        sign = -sign;             // Flip the sign
+    }
 
-    printf(" π = %.5f\n", pi);
+    printf("π = %.5f\n", pi);
+    printf("Number of terms (x) = %d\n", x);
 
     return 0;
 }
+
